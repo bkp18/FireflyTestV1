d23e0614a0a46e66257b2d4031f6014920de675b
bkp18 <43169378+bkp18@users.noreply.github.com>
2020-09-12 22:46:07 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
 basic.forever(function () {
-    basic.showNumber(2)
-    Firefly.setDC(Firefly.DCPinValues.dcmotor4, Firefly.DCDirectionValues.forward, 84)
-    basic.pause(1000)
-    Firefly.setDC(Firefly.DCPinValues.dcmotor4, Firefly.DCDirectionValues.forward, 0)
-    basic.pause(1000)
+	
 })
