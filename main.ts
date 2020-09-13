56427f74c142a93739cd9343a73f8a4da2b0954d
bkp18 <43169378+bkp18@users.noreply.github.com>
2020-09-13 16:46:34 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
+})
+basic.forever(function () {
+    Firefly.setDC(Firefly.DCPinValues.dcmotor1, Firefly.DCDirectionValues.forward, 0)
 })
